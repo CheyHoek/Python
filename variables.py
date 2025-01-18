f6e246ac488abd8f8b1acca9e32daecd324c0e5c
CheyHoek <al222210587@gmail.com>
2025-01-18 13:38:53 -0600

    upload
    Versión 1

---
+++ variables.py
@@ -0,0 +1,31 @@
+#entero = 10 #Número ente
+#print(entero)
+#decimal = 10.5 #Número decimal o flotante
+#texto = "Hola python" #Texto o cadena de texto
+#booleano = True #Variable booleana (true o false)
+
+#Imprimir variables y tipo de dato
+#print("Entero: ", entero, "Tipo de dato: ", type(entero))
+#print("Decimal: ", decimal, "Tipo de dato: ", type(decimal))
+#print("Texto: ", texto, "Tipo de dato: ", type(texto))
+#print("Booleano: ", booleano, "Tipo de dato: ", type(booleano))
+
+#Cambio de tipo de dato
+#entero = float(entero)
+#decimal = int(decimal)
+#texto = str(texto)
+#booleano = not booleano
+
+#Ejercicio prático
+#nombre = "Isabel"
+#edad = 31
+#learning = True
+# 
+#print("Hola, me llamo", nombre, "tengo", edad, "estoy aprendiendo python", learning)
+
+#Ejercicio prácico 2
+nombre = input("Cuál es tu nombre?: ")
+edad = input ("Cuál es tu edad?: ")
+learning = input("Estás aprendiendo python?: ")
+
+print(f"Hola, me llamo {nombre}, tengo {edad} años, estoy aprendiendo python {learning}")
